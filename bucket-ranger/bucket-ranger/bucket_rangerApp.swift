5d2d5f0977506eae960950548fa7c1341046f010
Jonathan Horbund <jonathan.horbund@gmail.com>
2026-03-23 22:44:13 -0400

    Created SwiftUI app

---
+++ bucket-ranger/bucket-ranger/bucket_rangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bucket_rangerApp.swift
+//  bucket-ranger
+//
+//  Created by Jonathan Horbund on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct bucket_rangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
